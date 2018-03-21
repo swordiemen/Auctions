592cac6f221527a7e8b90ebf73aaf1a425edd254
Tim <tim.blokk@gmail.com>
2018-03-21 01:50:57 +0100

    Fixed bids not serializing to the proper file. Added serialization for UserService. Fixed some formatting.

--- AuctionService/src/org/example/www/auction/AuctionServiceSkeleton.java
+++ AuctionService/src/org/example/www/auction/AuctionServiceSkeleton.java
@@ -7,7 +7,16 @@
  */
 package org.example.www.auction;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -15,7 +24,9 @@ import java.util.Set;
  */
 public class AuctionServiceSkeleton implements AuctionServiceSkeletonInterface {
 
-private Set<AuctionItem> items = new HashSet<>();
+	private Set<AuctionItem_type0> items = new HashSet<>();
+	private Map<AuctionItem_type0, Bid> bids = new HashMap<>();
+
 	/**
 	 * Auto generated method signature
 	 * 
@@ -23,13 +34,52 @@ private Set<AuctionItem> items = new HashSet<>();
 	 * @return param4
 	 * @throws BidItemFault
 	 */
-
-	public void bidItem(org.example.www.auction.Bid bid3) throws BidItemFault {
-		// TODO : fill this with the necessary business logic
-
+	
+	public AuctionServiceSkeleton() {
+		load();
+	}
+	
+	public void save() {
+		try(ObjectOutputStream oosItems = new ObjectOutputStream(new FileOutputStream(new File("items.obj"))); 
+				ObjectOutputStream oosBids = new ObjectOutputStream(new FileOutputStream(new File("bids.obj")))) {
+			oosItems.writeObject(getItems());
+			oosBids.writeObject(getBids());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void load() {
+		try(ObjectInputStream oosItems = new ObjectInputStream(new FileInputStream(new File("items.obj"))); 
+				ObjectInputStream oosBids = new ObjectInputStream(new FileInputStream(new File("bids.obj")))) {
+			this.items = (Set<AuctionItem_type0>) oosItems.readObject();
+			this.bids = (Map<AuctionItem_type0, Bid>) oosBids.readObject();
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
+	public void bidItem(org.example.www.auction.Bid bid3) throws BidItemFault {
+		AuctionItem_type0 ai = bid3.getAuctionItem();
+		Bid currentBid = getBids().get(ai);
+		if(currentBid == null && !getItems().contains(bid3.getAuctionItem())) {
+			throw new BidItemFault("The bid's AuctionItem does not exist!");
+		} else if(bid3.getBidAmount() > bid3.getBidAmount()) {
+			throw new BidItemFault("Current bid is higher than the given bid!");
+		} else {
+			getBids().put(ai, bid3);
+			save();
+		}
+	}
 	
+	public Map<AuctionItem_type0, Bid> getBids() {
+		return bids;
+	}
+	
+	public Set<AuctionItem_type0> getItems() {
+		return items;
+	}
+
 	/**
 	 * Auto generated method signature
 	 * 
@@ -39,10 +89,13 @@ private Set<AuctionItem> items = new HashSet<>();
 	 */
 
 	public void addAuction(org.example.www.auction.AuctionItem auctionItem) throws AddAuctionFault {
-		if(items.stream().anyMatch(au -> au.getAuctionItem().getItemName().equals(auctionItem.getAuctionItem().getItemName()))) {
+		if (items.stream()
+				.anyMatch(au -> au.getItemName().equals(auctionItem.getAuctionItem().getItemName())
+						&& au.getItemOwnerName().equals(auctionItem.getAuctionItem().getItemOwnerName()))) {
 			throw new AddAuctionFault("Auction item already exists!");
 		} else {
-			items.add(auctionItem);
+			items.add(auctionItem.getAuctionItem());
+			save();
 		}
 
 	}
@@ -57,10 +110,9 @@ private Set<AuctionItem> items = new HashSet<>();
 	public org.example.www.auction.ItemList getAllAuctionItems(
 
 	) throws GetAllAuctionItemsFault {
-		// TODO : fill this with the necessary business logic
 		ItemList il = new ItemList();
 		items.stream().forEach(au -> {
-			il.addAuctionItem(au.getAuctionItem());
+			il.addAuctionItem(au);
 		});
 		return il;
 	}
--- UserService/src/org/example/www/user/UserServiceSkeleton.java
+++ UserService/src/org/example/www/user/UserServiceSkeleton.java
@@ -7,12 +7,46 @@
  */
 package org.example.www.user;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.www.user.getuserinfo.GetUserInfoResponse;
 
 /**
  * UserServiceSkeleton java skeleton for the axisService
  */
 public class UserServiceSkeleton implements UserServiceSkeletonInterface {
+	
+	public List<AddUser> users = new ArrayList<>();
+	
+	public UserServiceSkeleton() {
+		load();
+	}
+	
+	public void save() {
+		try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File("users.obj")))) {
+			oos.writeObject(getUsers());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void load() {
+		try(ObjectInputStream oos = new ObjectInputStream(new FileInputStream(new File("users.obj")))) {
+			this.users = (List<AddUser>) oos.readObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Auto generated method signature
@@ -24,11 +58,11 @@ public class UserServiceSkeleton implements UserServiceSkeletonInterface {
 
 	public com.example.www.user.getuserinfo.GetUserInfoResponse getUserInfo(
 			org.example.www.user.GetUserInfo getUserInfo0) throws UserFaultException {
-		
-		if(getUserInfo0.getUsername().equals("admin")) {
+		AddUser user = getUserByName(getUserInfo0.getUsername());
+		if (user != null) {
 			GetUserInfoResponse aur = new GetUserInfoResponse();
-			aur.setAddress("adminlaan 2000");
-			aur.setEmail("admin@admin.admin");
+			aur.setAddress(user.getAddress());
+			aur.setEmail(user.getEmail());
 			return aur;
 		} else {
 			throw new UserFaultException("Unkown username.");
@@ -38,15 +72,29 @@ public class UserServiceSkeleton implements UserServiceSkeletonInterface {
 	/**
 	 * Auto generated method signature
 	 * 
-	 * @param addUser2
+	 * @param addUser
 	 * @return addUserResponse3
 	 * @throws UserFaultException
 	 */
 
-	public org.example.www.user.AddUserResponse addUser(org.example.www.user.AddUser addUser2)
+	public org.example.www.user.AddUserResponse addUser(org.example.www.user.AddUser addUser)
 			throws UserFaultException {
-		// TODO : fill this with the necessary business logic
-		throw new java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#addUser");
+		AddUser curUser = getUserByName(addUser.getUsername()); 
+		if(curUser != null) {
+			getUsers().add(addUser);
+			save();
+			return new AddUserResponse();	
+		} else {
+			throw new UserFaultException("User with that username already exists.");
+		}
+	}
+	
+	public List<AddUser> getUsers() {
+		return users;
+	}
+	
+	public AddUser getUserByName(String name) {
+		return getUsers().stream().filter(u -> u.getUsername().equals(name)).findFirst().orElse(null);
 	}
 
 }
